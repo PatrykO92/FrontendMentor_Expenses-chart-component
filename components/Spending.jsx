3ac8f471da6582096f34a6c1960d020944da4bd5
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2023-06-04 18:43:47 +0200

    init: initial commit

---
+++ components/Spending.jsx
@@ -0,0 +1,11 @@
+const Spending = () => {
+  return (
+    <div className="flex items-center p-5 text-dark-brown rounded-2xl justify-between bg-white min-w-[20rem]">
+      <div>
+        <p className="font-bold">Spending - Last 7 days</p>
+      </div>
+    </div>
+  );
+};
+
+export default Spending;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      "soft-red": "hsl(10, 79%, 65%)",
+      cyan: "hsl(186, 34%, 60%)",
+      "dark-brown": "hsl(25, 47%, 15%)",
+      "medium-brown": "hsl(28, 10%, 53%)",
+      cream: "hsl(27, 66%, 92%)",
+      "very-pale-orange": "hsl(33, 100%, 98%)",
     },
   },
   plugins: [],
-}
+};
